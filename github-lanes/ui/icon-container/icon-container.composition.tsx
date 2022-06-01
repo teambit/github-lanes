cd89af3b154605f446eed0f09376b822c60c2dc3
Luv Kapur <luv@bit.dev>
2022-06-01 08:02:23 -0400

    uncomment workflows

---
+++ github-lanes/ui/icon-container/icon-container.composition.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IconContainer } from './icon-container';
+
+export const BasicIconContainer = () => {
+  return (
+    <IconContainer></IconContainer>
+  );
+}
---
+++ github-lanes/ui/icon/icon.composition.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Icon } from './icon';
+
+export const BasicIcon = () => {
+  return (
+    <Icon></Icon>
+  );
+}
