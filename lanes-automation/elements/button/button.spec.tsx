efe850203d3af5186a21624970f603be547598ba
itay mendelawy <itay.mendelawy@gmail.com>
2023-02-26 20:12:14 +0000

    removing tests

--- lanes-automation/elements/button/button.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicButton } from './button.composition';
-
-it('renders with the correct text', () => {
-  const { getByText } = render(<BasicButton />);
-  const rendered = getByText('hello world!');
-  expect(rendered).toBeTruthy();
-});
--- lanes-automation/styles/colors/colors.spec.ts
+++
@@ -1,5 +0,0 @@
-import { colors } from './colors';
-
-it('renders with the correct text', () => {
-  expect(colors()).toBe('hello world');
-});
